128b6e0aeaad8ec1a8e5dfdb080ddff2b54309ef
Sorawit Trutsat (Kong) <sorawit.tr@gmail.com>
2023-11-23 11:55:05 +0700

    feat: Add WidgetKit

---
+++ MoodyWidget/MoodyWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  MoodyWidgetBundle.swift
+//  MoodyWidget
+//
+//  Created by Sorawit Trutsat on 23/11/2566 BE.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct MoodyWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        MoodyWidget()
+    }
+}
